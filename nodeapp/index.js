f606c86efef372d0d5e9943e474147ff948fa3db
Chamodpw2000 <143950123+Chamodpw2000@users.noreply.github.com>
2025-02-04 23:46:41 +0530

    Update index.js

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send('{ "response": "Hello, Welcome to Adomic Arts samin chamod" }');
+  res.send('{ "response": "Hello, Welcome to Adomic Arts samin chamod chamod" }');
 });
 
 app.get("/will", function (req, res) {
